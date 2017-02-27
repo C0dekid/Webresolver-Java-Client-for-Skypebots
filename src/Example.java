ceafe9ed47ce35f57cc64b50dd3604494222f772
Jer <tweetme@live.nl>
2017-02-27 15:22:31 +0100

    Example test 'ping'

--- src/Example.java
+++ src/Example.java
@@ -10,30 +10,17 @@ public class Example {
 	{
 		WebresolverAPI client = new WebresolverAPI();
 		try {
-			client.SetKey("53CDX-ULBMX-9GYA9-K615P");
+			client.setKey("XXXXX-XXXXX-XXXXX-XXXXX");
 			client.setJson(true);
 			client.setUseragent("Skypebot/1.0 (Windows NT 5.1; rv:19.0) Gecko/20100101 Firefox/19.0");
+			// client.port(443);
 			
-			if(client.isJson() == true)
-			{
-				JSONObject j = new JSONObject(client.Request("echo123", "resolve"));
-				
-				if(!j.getBoolean("success"))
-				{
-					System.out.print(j.getString("error"));
-					
-				}else{
-					System.out.println(j.getString("ip"));
-				}
-			}
+			System.out.println(client.Request("https://www.webresolver.nl", "ping").replace("<br />", "\r\n"));
 			
 		} catch (IOException e) {
 			
 			e.printStackTrace();
 			
-		} catch (JSONException e) {
-			
-			e.printStackTrace();
 		}
 	}
 }
